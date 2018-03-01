831bc3aa3850bba849f0f7c27f967557d2c01fab
CJ R <cj@null.computer>
2018-03-01 13:35:44 -0700

    :sparkles: Add body-parser

--- drills/version-2/de-coupled/server/index.js
+++ drills/version-2/de-coupled/server/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const cors = require('cors');
 
 const app = express();
 
 app.use(morgan('tiny'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(cors());
 
 app.get('/', (req, res) => {
